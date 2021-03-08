04a2b1dd94a468287ce0ed15a64464d7e5e89ae3
Suraj Prasad <surajvishwakarma830@gmail.com>
2021-03-08 23:46:35 +0530

    herader updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,45 @@
 import React from 'react';
+import {StyleSheet, View, Text, TextInput} from 'react-native';
+import {Icon} from 'react-native-elements';
+
+const Header = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <View style={{margin: 10}}>
+        <Text>What movie are you looking</Text>
+        <Text style={{fontWeight: 'bold'}}>forward to seeing ?</Text>
+      </View>
+      <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+        <View
+          style={{
+            borderRadius: 10,
+            width: width * 0.7,
+            backgroundColor: '#f2f2f2',
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}>
+          <Icon name="search" type="evilicon" />
+          <TextInput
+            placeholder="Search"
+            style={{width: '90%'}}
+            onFocus={() => navigation.navigate('Search')}
+          />
+        </View>
+        <Button
+          title="+33"
+          buttonStyle={{backgroundColor: 'red'}}
+          titleStyle={{color: 'white'}}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
+
+export default Header;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -18,7 +18,7 @@ import {
   fetchToprated,
 } from '../redux/actions/movieAction';
 
-import {Title} from '../components';
+import {Header, Title} from '../components';
 const {width} = Dimensions.get('window');
 
 import {Card} from '../components';
@@ -39,74 +39,46 @@ const Home = ({
   }, []);
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={{margin: 10}}>
-          <Text>What movie are you looking</Text>
-          <Text style={{fontWeight: 'bold'}}>forward to seeing ?</Text>
-        </View>
-        <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
-          <View
-            style={{
-              borderRadius: 10,
-              width: width * 0.7,
-              backgroundColor: '#f2f2f2',
-              flexDirection: 'row',
-              alignItems: 'center',
-            }}>
-            <Icon name="search" type="evilicon" />
-            <TextInput
-              placeholder="Search"
-              style={{width: '90%'}}
-              onFocus={() => navigation.navigate('Search')}
-            />
-          </View>
-          <Button
-            title="+33"
-            buttonStyle={{backgroundColor: 'red'}}
-            titleStyle={{color: 'white'}}
-          />
-        </View>
+    <ScrollView style={{flex: 1}}>
+      <Header navigation={navigation} />
+      {/*Trending*/}
+      <Title navigation={navigation}>Trending</Title>
+      <FlatList
+        data={trending}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(_, index) => index.toString()}
+        scrollEnabled
+        renderItem={({item, index}) => {
+          return <Card item={item} navigation={navigation} key={index} />;
+        }}
+      />
 
-        {/*Trending*/}
-        <Title navigation={navigation}>Trending</Title>
-        <FlatList
-          data={trending}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          keyExtractor={(_, index) => index.toString()}
-          scrollEnabled
-          renderItem={({item, index}) => {
-            return <Card item={item} navigation={navigation} key={index} />;
-          }}
-        />
+      {/*Upcoming*/}
+      <Title navigation={navigation}>Upcoming</Title>
+      <FlatList
+        data={upcoming}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        scrollEnabled
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({item, index}) => {
+          return <Card item={item} navigation={navigation} key={index} />;
+        }}
+      />
 
-        {/*Upcoming*/}
-        <Title navigation={navigation}>Upcoming</Title>
-        <FlatList
-          data={upcoming}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          scrollEnabled
-          keyExtractor={(_, index) => index.toString()}
-          renderItem={({item, index}) => {
-            return <Card item={item} navigation={navigation} key={index} />;
-          }}
-        />
-
-        {/**Top Rated */}
-        <Title navigation={navigation}>Top Rated</Title>
-        <FlatList
-          data={top_rated}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          scrollEnabled
-          keyExtractor={(_, index) => index.toString()}
-          renderItem={({item, index}) => {
-            return <Card item={item} navigation={navigation} key={index} />;
-          }}
-        />
-      </View>
+      {/**Top Rated */}
+      <Title navigation={navigation}>Top Rated</Title>
+      <FlatList
+        data={top_rated}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        scrollEnabled
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({item, index}) => {
+          return <Card item={item} navigation={navigation} key={index} />;
+        }}
+      />
     </ScrollView>
   );
 };
